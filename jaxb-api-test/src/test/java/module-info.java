6ed71429a20f4c89d67fb7083fb6badf1a9972aa
Thibault Vallin <thibault.vallin@oracle.com>
2020-01-17 12:00:54 +0100

    Rename package from javax to jakarta
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- jaxb-api-test/src/test/java/module-info.java
+++ jaxb-api-test/src/test/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -11,8 +11,8 @@
 /**
  * Tests for jaxb API.
  */
-module java.xml.bind.tests {
-    requires java.xml.bind;
+module jakarta.xml.bind.tests {
+    requires jakarta.xml.bind;
     requires java.logging;
     requires junit;
 }
---
+++ jaxb-api/src/main/java/jakarta/xml/bind/helpers/PrintConversionEventImpl.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2003, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package jakarta.xml.bind.helpers;
+
+import jakarta.xml.bind.PrintConversionEvent;
+import jakarta.xml.bind.ValidationEventLocator;
+
+/**
+ * Default implementation of the PrintConversionEvent interface.
+ * 
+ * <p>
+ * JAXB providers are allowed to use whatever class that implements
+ * the ValidationEvent interface. This class is just provided for a
+ * convenience.
+ *
+ * @author <ul><li>Ryan Shoemaker, Sun Microsystems, Inc.</li></ul> 
+ * @see jakarta.xml.bind.PrintConversionEvent
+ * @see jakarta.xml.bind.Validator
+ * @see jakarta.xml.bind.ValidationEventHandler
+ * @see jakarta.xml.bind.ValidationEvent
+ * @see jakarta.xml.bind.ValidationEventLocator
+ * @since 1.6, JAXB 1.0
+ */
+public class PrintConversionEventImpl
+    extends ValidationEventImpl
+    implements PrintConversionEvent {
+
+    /**
+     * Create a new PrintConversionEventImpl.
+     * 
+     * @param _severity The severity value for this event.  Must be one of
+     * ValidationEvent.WARNING, ValidationEvent.ERROR, or 
+     * ValidationEvent.FATAL_ERROR
+     * @param _message The text message for this event - may be null.
+     * @param _locator The locator object for this event - may be null.
+     * @throws IllegalArgumentException if an illegal severity field is supplied
+     */
+    public PrintConversionEventImpl( int _severity, String _message,
+                                      ValidationEventLocator _locator) {
+            
+        super(_severity, _message, _locator);
+    }
+
+    /**
+     * Create a new PrintConversionEventImpl.
+     * 
+     * @param _severity The severity value for this event.  Must be one of
+     * ValidationEvent.WARNING, ValidationEvent.ERROR, or 
+     * ValidationEvent.FATAL_ERROR
+     * @param _message The text message for this event - may be null.
+     * @param _locator The locator object for this event - may be null.
+     * @param _linkedException An optional linked exception that may provide
+     * additional information about the event - may be null.
+     * @throws IllegalArgumentException if an illegal severity field is supplied
+     */
+    public PrintConversionEventImpl( int _severity, String _message,
+                                      ValidationEventLocator _locator,
+                                      Throwable _linkedException) {
+            
+        super(_severity, _message, _locator, _linkedException);
+    }
+
+}
