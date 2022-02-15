2c22f9e5dbaa7a73ea42ee8531fd30b6bb8edfbf
Maxim Nesen <maxim.nesen@oracle.com>
2022-02-15 12:54:30 +0100

    Jenkins release job update
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- etc/scripts/nexus.incl.sh
+++ etc/scripts/nexus.incl.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2022 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -12,7 +12,7 @@
 #  $2 - Build directory
 drop_artifacts() {
   echo '-[ Drop old staging repository deployments ]------------------------------------'
-  for staging_key in `(cd ${2} && mvn -B nexus-staging:rc-list | egrep "^\[INFO\] [A-Z,a-z,-]+-[0-9]+\s+[A-Z]+\s+${1}" | awk '{print $2}')`; do
+  for staging_key in `(cd ${2} && mvn -B nexus-staging:rc-list | egrep "^\[INFO\] [A-Z,a-z,-]+-[0-9]+\s+[A-Z]+\s+${1}\$" | awk '{print $2}')`; do
     echo "Repository ID: ${staging_key}"
     (cd ${2} && \
       mvn -U -C \
