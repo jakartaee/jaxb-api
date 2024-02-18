b38b15344c9f0647242c4731dd10e58f0a8edbd6
Lukas Jungmann <lukas.jungmann@oracle.com>
2024-02-18 13:50:30 +0100

    build by 21
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- etc/jenkins/continuous.groovy
+++ etc/jenkins/continuous.groovy
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, 2022 Oracle and/or its affiliates. All rights reserved.
+// Copyright (c) 2019, 2024 Oracle and/or its affiliates. All rights reserved.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -26,7 +26,7 @@ pipeline {
     agent any
 
     tools {
-        jdk 'openjdk-jdk11-latest'
+        jdk 'openjdk-jdk21-latest'
         maven 'apache-maven-latest'
     }
 
