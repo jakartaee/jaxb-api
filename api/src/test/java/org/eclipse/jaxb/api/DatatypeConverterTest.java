4525821e5ee855b0e93e6e9486b954b0fcd44f75
Lukas Jungmann <1069133+lukasj@users.noreply.github.com>
2026-03-27 12:33:44 +0100

    bump src level to SE 17

--- api/src/test/java/org/eclipse/jaxb/api/DatatypeConverterTest.java
+++ api/src/test/java/org/eclipse/jaxb/api/DatatypeConverterTest.java
@@ -1,6 +1,6 @@
 /*
+ * Copyright (c) 2025, 2026 Contributors to the Eclipse Foundation. All rights reserved.
  * Copyright (c) 2023, 2024 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2025 Contributors to the Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -12,95 +12,95 @@
 package org.eclipse.jaxb.api;
 
 import jakarta.xml.bind.DatatypeConverter;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class DatatypeConverterTest {
 
     @Test
     public void testParseBoolean() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean(""));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("11"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("1A"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("non"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("fals"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("falses"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("false s"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("falst"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("tru"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("trux"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truu"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truxx"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truth"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truelle"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truec"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("true c"));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("oui"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean(""));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("11"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("1A"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("non"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("fals"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("falses"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("false s"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("falst"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("tru"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("trux"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truu"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truxx"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truth"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truelle"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("truec"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("true c"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBoolean("oui"));
 
 
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean("0"));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean(" 0"));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean(" 0 "));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean("0 "));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean("1"));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean(" 1"));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean(" 1 "));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean("1 "));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean("false"));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean(" false"));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean("false "));
-        Assert.assertEquals(false, DatatypeConverter.parseBoolean(" false "));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean("true"));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean(" true"));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean("true "));
-        Assert.assertEquals(true, DatatypeConverter.parseBoolean(" true "));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean("0"));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean(" 0"));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean(" 0 "));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean("0 "));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean("1"));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean(" 1"));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean(" 1 "));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean("1 "));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean("false"));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean(" false"));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean("false "));
+        Assertions.assertEquals(false, DatatypeConverter.parseBoolean(" false "));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean("true"));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean(" true"));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean("true "));
+        Assertions.assertEquals(true, DatatypeConverter.parseBoolean(" true "));
     }
 
     @Test 
     public void testPrint() { 
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printInteger(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDateTime(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printHexBinary(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printTime(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDate(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDecimal(null));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printBase64Binary(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printInteger(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDateTime(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printHexBinary(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printTime(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDate(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDecimal(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printBase64Binary(null));
         
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printShort(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printFloat(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printBoolean(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printByte(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printUnsignedInt(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printString(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printInt(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printLong(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDouble(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printQName(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printUnsignedShort(null));
-        //Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printAnySimpleType(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printShort(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printFloat(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printBoolean(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printByte(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printUnsignedInt(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printString(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printInt(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printLong(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printDouble(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printQName(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printUnsignedShort(null));
+        //Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.printAnySimpleType(null));
         
     } 
 
     @Test
     public void testBase64() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBase64Binary("Qxx=="));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBase64Binary("SGVsbG8sIJdvcmxkIQQxx=="));
-        Assert.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBase64Binary("dGhpcyBpcyB\nhbiBleGFtcGxl=="));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBase64Binary("Qxx=="));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBase64Binary("SGVsbG8sIJdvcmxkIQQxx=="));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DatatypeConverter.parseBase64Binary("dGhpcyBpcyB\nhbiBleGFtcGxl=="));
 
-        Assert.assertEquals("", new String(DatatypeConverter.parseBase64Binary("")));
-        Assert.assertEquals("f", new String(DatatypeConverter.parseBase64Binary("Zg==")));
-        Assert.assertEquals("fo", new String(DatatypeConverter.parseBase64Binary("Zm8=")));
-        Assert.assertEquals("foo", new String(DatatypeConverter.parseBase64Binary("Zm9v")));
-        Assert.assertEquals("foob", new String(DatatypeConverter.parseBase64Binary("Zm9vYg==")));
-        Assert.assertEquals("fooba", new String(DatatypeConverter.parseBase64Binary("Zm9vYmE=")));
-        Assert.assertEquals("foobar", new String(DatatypeConverter.parseBase64Binary("Zm9vYmFy")));
-        Assert.assertEquals("this is an example", new String(DatatypeConverter.parseBase64Binary("dGhpcyBpcyB hbiBleGFtcGxl")));
-        Assert.assertEquals("this is an example", new String(DatatypeConverter.parseBase64Binary("dGhpcyBpcyB\nhbiBleGFtcGxl")));
-        Assert.assertEquals("this is an example", new String(DatatypeConverter.parseBase64Binary("dGhpcyBpcyB\thbiBleGFtcGxl")));
+        Assertions.assertEquals("", new String(DatatypeConverter.parseBase64Binary("")));
+        Assertions.assertEquals("f", new String(DatatypeConverter.parseBase64Binary("Zg==")));
+        Assertions.assertEquals("fo", new String(DatatypeConverter.parseBase64Binary("Zm8=")));
+        Assertions.assertEquals("foo", new String(DatatypeConverter.parseBase64Binary("Zm9v")));
+        Assertions.assertEquals("foob", new String(DatatypeConverter.parseBase64Binary("Zm9vYg==")));
+        Assertions.assertEquals("fooba", new String(DatatypeConverter.parseBase64Binary("Zm9vYmE=")));
+        Assertions.assertEquals("foobar", new String(DatatypeConverter.parseBase64Binary("Zm9vYmFy")));
+        Assertions.assertEquals("this is an example", new String(DatatypeConverter.parseBase64Binary("dGhpcyBpcyB hbiBleGFtcGxl")));
+        Assertions.assertEquals("this is an example", new String(DatatypeConverter.parseBase64Binary("dGhpcyBpcyB\nhbiBleGFtcGxl")));
+        Assertions.assertEquals("this is an example", new String(DatatypeConverter.parseBase64Binary("dGhpcyBpcyB\thbiBleGFtcGxl")));
 
-        Assert.assertNotEquals("Hello, world!", new String(DatatypeConverter.parseBase64Binary("SGVsbG8sIJdvcmxkIQ==")));
+        Assertions.assertNotEquals("Hello, world!", new String(DatatypeConverter.parseBase64Binary("SGVsbG8sIJdvcmxkIQ==")));
 
-        Assert.assertEquals("Hello, world!", new String(DatatypeConverter.parseBase64Binary("SGVsbG8sIHdvcmxkIQ==")));
+        Assertions.assertEquals("Hello, world!", new String(DatatypeConverter.parseBase64Binary("SGVsbG8sIHdvcmxkIQ==")));
     }
 }
